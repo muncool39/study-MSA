bb0d33fd3705d6786aa6bb4ee4e9e46e8b390963
시원 <105481797+muncool39@users.noreply.github.com>
2024-11-24 17:20:23 +0900

    [Practice] API Gateway - Spring Cloud Gateway (#4)

---
+++ API Gateway API 게이트웨이/com.spring-cloud.eureka.client.order/src/main/java/com/spring_cloud/eureka/client/order/OrderController.java
@@ -0,0 +1,15 @@
+package com.spring_cloud.eureka.client.order;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/order")
+public class OrderController {
+
+    @GetMapping
+    public String getOrder() {
+        return "Order details";
+    }
+}
