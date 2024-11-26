ee20f7e1f9122363a31364b6d00281549ce548b4
시원 <105481797+muncool39@users.noreply.github.com>
2024-11-26 12:42:56 +0900

    [Practice] Distributed Configuration - Spring Cloud Config (#6)
    * [Practice] Distributed Configuration - Spring Cloud Config
    
    * [Docs] update README.md

---
+++ Distributed Configuration 분산 설정/com.spring-cloud.eureka.server/src/main/java/com/spring_cloud/eureka/sever/SeverApplication.java
@@ -0,0 +1,15 @@
+package com.spring_cloud.eureka.sever;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class SeverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeverApplication.class, args);
+	}
+
+}
